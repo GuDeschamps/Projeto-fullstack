c872b7e961ab26bdf205b4596cc3fc66c254cc80
Gustavo <gustavodguilherme0205@gmail.com>
2023-04-05 03:12:49 -0300

    attAbaProjetos

---
+++ botoes.js
@@ -0,0 +1,15 @@
+function myFunction() {
+
+    document.getElementById("descricaoProjeto").innerHTML = "Projeto alura midi, utilizando javascrip criamos uma bateria.";
+
+    document.getElementById("imagemProjeto").style.display = "none";
+
+    document.getElementById("Pessoas-programando").style.display = "block";
+
+  }
+
+  function myFunction2() {
+    document.getElementById("descricaoProjeto").innerHTML = "Projeto Freeway, jogo";
+    document.getElementById("imagemProjeto").style.display = "block";
+    document.getElementById("Pessoas-programando").style.display = "none";
+  }
